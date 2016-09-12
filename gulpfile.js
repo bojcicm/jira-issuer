d19694f12693f2666dc36c0261df3c438bea9225
Mirko Bojcic <mbojcic@neogov.net>
2016-09-12 13:16:50 +0200

    yes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 run = require('gulp-run'),
-livereload = require('gulp-livereload')
+livereload = require('gulp-livereload');
+var winInstaller = require('electron-windows-installer');
 
 var appFolder = ['scripts/**/*.js', 'scripts/**/*.html','styles/*.css', '*.html', '*.js'];
 
@@ -18,5 +19,14 @@ gulp.task('watch', function(){
 gulp.task('run', function(){
     return run('electron .').exec();
 })
+
+gulp.task('create-windows-installer', function(done) {
+  winInstaller({
+    appDirectory: './releases/jira-issuer-win32-x64',
+    outputDirectory: './install',
+    authors: 'mbojcic',
+    setupIcon: 'file.ico'
+  }).then(done).catch(done);
+});
 
 gulp.task('default', ['watch', 'run']);
